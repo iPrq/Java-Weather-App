d505b97e630cb1d0f59e6ecf193dbe24152ad4c5
Neeraj Ciju <danciju@gmail.com>
2024-12-04 23:41:06 +0530

    Initial Commit

---
+++ src/main/java/main/weatherapp/Main.java
@@ -0,0 +1,9 @@
+package main.weatherapp;
+
+import static main.weatherapp.HelloApplication.run;
+
+public class Main {
+    public static void main(String[] args) {
+    ApiClass.apicall();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module main.weatherapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires io.github.cdimascio.dotenv.java;
+    requires java.net.http;
+
+    opens main.weatherapp to javafx.fxml;
+    exports main.weatherapp;
+}
