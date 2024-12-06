6d25f4d0792acff39ae1f4e301eda61fc82c7020
Neeraj Ciju <danciju@gmail.com>
2024-12-06 19:35:28 +0530

    Implimented Background changes with weather

--- src/main/java/main/weatherapp/MainAppController.java
+++ src/main/java/main/weatherapp/MainAppController.java
@@ -2,6 +2,7 @@ package main.weatherapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -58,6 +59,7 @@ public class MainAppController {
     private  ApiClass Api ;
     private String time;
     private boolean day;
+    private Image image;
 
     public void init(Stage stage) {
         titlePane.setOnMousePressed(MouseEvent -> {
@@ -90,7 +92,7 @@ public class MainAppController {
             templbl.setText(String.valueOf(weatherdata.get("temp")));
             maxtemplbl.setText(String.valueOf(weatherdata.get("temp_max")));
             mintemplbl.setText(String.valueOf(weatherdata.get("temp_min")));
-            humlbl.setText(String.valueOf(weatherdata.get("humidity")));
+            humlbl.setText(String.valueOf(weatherdata.get("humidity"))+"%");
             weatherbg(String.valueOf(weatherdata.get("main_weather")));
 
             time = returndaynight((Integer) weatherdata.get("timezone"));
@@ -105,41 +107,74 @@ public class MainAppController {
 
     }
     public void weatherbg(String weather) {
+        day = false;
         if (!day) {
             switch (weather) {
                 case "Thunderstorm":
-                    break;
-                case "Haze":
+                    image = new Image("file:src/main/resources/images/weathericons/thunderstormday.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #0a0a0a, #3a3a3a)");
                     break;
                 case "Rain":
+                    image = new Image("file:src/main/resources/images/weathericons/rainyday.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #647d8e, #3a506b, #2c3e50);");
                     break;
                 case "Snow":
+                    image = new Image("file:src/main/resources/images/weathericons/snowday.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #ffffff, #d0e6f6, #a9cce3, #83b4d0);;");
                     break;
                 case "Clear":
+                    image = new Image("file:src/main/resources/images/weathericons/clearday.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #87CEFA, #00BFFF, #1E90FF);");
                     break;
                 case "Clouds":
+                    image = new Image("file:src/main/resources/images/weathericons/cloudday.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #B0C4DE, #D3D3D3, #A9A9A9);");
                     break;
                 default:
+                    image = new Image("file:src/main/resources/images/weathericons/mistday.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("linear-gradient(to bottom, #505050, #a9a9a9, #bdbbbb);");
                     break;
             }
         }
         else {
             switch (weather) {
                 case "Thunderstorm":
-                    break;
-                case "Haze":
+                    image = new Image("file:src/main/resources/images/weathericons/thunderstormnight.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #0a0a0a, #3a3a3a)");
                     break;
                 case "Rain":
+                    image = new Image("file:src/main/resources/images/weathericons/rainynight.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #1c1c1c, #2c3e50, #3a506b, #2c3e50);");
                     break;
                 case "Snow":
+                    image = new Image("file:src/main/resources/images/weathericons/snownight.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #0a0a0a, #1c1c1c, #3a3a3a, #87CEFA);");
                     break;
                 case "Clear":
+                    image = new Image("file:src/main/resources/images/weathericons/clearnight.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #000033, #000066, #000099, #0000cc);");
                     break;
                 case "Clouds":
+                    image = new Image("file:src/main/resources/images/weathericons/cloudnight.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("-fx-background-color: linear-gradient(to bottom, #2c3e50, #3a3a3a, #1c1c1c);");
                     break;
                 default:
+                    image = new Image("file:src/main/resources/images/weathericons/mistday.png");
+                    weather_icon.setImage(image);
+                    mainPane.setStyle("linear-gradient(to bottom, #505050, #a9a9a9, #bdbbbb);");
                     break;
-            }
+                    }
         }
     }
     String returndaynight(int timezone) {
